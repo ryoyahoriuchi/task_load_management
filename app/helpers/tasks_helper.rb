28d463464ce092924f612cce313e11447add8e00
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-08 00:06:42 +0900

    国際化(i18n)対応

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,14 +3,8 @@ module TasksHelper
     if action_name == "new" || action_name == "create"
       suggestion_tasks_path
     elsif action_name == "edit" || action_name == "update"
-      #task_path(@task.id)
       suggestion_task_path
     end
-    # unless @task.id?
-    #   suggestion_tasks_path
-    # else
-    #   suggestion_tasks_path(@task.id)
-    # end
   end
 
   def suggestion_new_or_edit
