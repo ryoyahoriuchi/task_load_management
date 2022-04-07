516d2813e52dfd39c25c9745f64ddcd382b76218
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-07 15:49:51 +0900

    Ajaxによるコメント編集・削除機能作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,10 +1,10 @@
 class MemosController < ApplicationController
+  before_action :set_task_item, only: %i[edit update destroy]
   def create
-    #@task = Task.find(params[:task_id])
+    @task = Task.find(params[:task_id])
     #@task_items = TaskItem.where(task_id: params[:task_id])
     @task_item = TaskItem.find(params[:memo][:task_item_id].to_i)
     @memo = @task_item.memos.build(memo_params)
-
     respond_to do |format|
       if @memo.save
         #format.html { redirect_to task_path(@task.id)}
@@ -15,10 +15,46 @@ class MemosController < ApplicationController
     end
   end
 
+  def edit
+    @memo = @task_item.memos.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = "メモ編集中"
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @memo = @task_item.memos.find(params[:id])
+    respond_to do |format|
+      if @memo.update(memo_params)
+        flash.now[:notice] = "メモを編集しました"
+        format.js { render :index }
+      else
+        flash.now[:notice] = "メモの編集に失敗しました"
+        format.js { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @memo = Memo.find(params[:id])
+    @memo.destroy
+    respond_to do |format|
+      flash.now[:notice] = "メモを削除しました"
+      format.js { render :index }
+    end
+  end
+
   private
 
   def memo_params
-    params.require(:memo).permit(:content, :id, :task_item_id )
+    params.require(:memo).permit(:content, :id, :task_item_id)
+  end
+
+  def set_task_item
+    memo = Memo.find(params[:id])
+    @task = Task.find(params[:task_id])
+    @task_item = TaskItem.find(memo.task_item.id)
   end
 
 end
