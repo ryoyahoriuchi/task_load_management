8382506a55fcf74ad1035ceba2f8bee6ed2c8104
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-11 11:27:11 +0900

    label system spec一部実装

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -2,9 +2,81 @@ require 'rails_helper'
 
 RSpec.describe 'Label function', type: :system do
 
+  let!(:first_label) { FactoryBot.create(:first_label) }
+  let!(:second_label) { FactoryBot.create(:second_label) }
+  let!(:third_label) { FactoryBot.create(:third_label) }
+  let!(:first_task) { FactoryBot.create(:first_task) }
+  let!(:first_task_item) { FactoryBot.create(:first_task_item, task: first_task) }
 
   before do
     visit root_path
+  end
+
+  describe 'ラベル取付機能' do
+    context 'タスク作成に伴いラベルを取り付けられる' do
+      it 'ラベルが表示される' do
+        click_link I18n.t('views.link.create_task')
+        fill_in 'task[title]', with: 'title_test'
+        fill_in 'task[overview]', with: 'overview_test'
+        fill_in 'task[task_items_attributes][0][item]', with: 'item_test'
+        check 'blue'
+        click_button I18n.t('helpers.submit.create')
+        click_button I18n.t('views.button.create')
+        expect(page).to have_content I18n.t('views.messages.create_task')
+        expect(page).to have_content 'blue'
+      end
+    end
+
+    context 'タスク作成に伴い複数ラベルを取り付けられる' do
+      it '複数ラベルが表示される' do
+        click_link I18n.t('views.link.create_task')
+        fill_in 'task[title]', with: 'title_test'
+        fill_in 'task[overview]', with: 'overview_test'
+        fill_in 'task[task_items_attributes][0][item]', with: 'item_test'
+        check 'blue'
+        check 'green'
+        click_button I18n.t('helpers.submit.create')
+        click_button I18n.t('views.button.create')
+        expect(page).to have_content I18n.t('views.messages.create_task')
+        expect(page).to have_content 'blue'
+        expect(page).to have_content 'green'
+      end
+    end
+  end
+
+  describe 'ラベル編集機能' do
+    before do
+      click_button I18n.t('views.button.edit')
+      check 'red'
+      click_button I18n.t('helpers.submit.update')
+      click_button I18n.t('views.button.create')
+    end
+    context 'タスクに紐づいているラベルを取付編集をした場合' do
+      it '該当ラベルも追加で表示される' do
+        expect(page).to have_content 'red'
+      end
+    end
+    
+    context 'タスクに紐づいているラベルを取り外した場合' do
+      it '該当ラベルは表示されなくなる' do
+        expect(page).to have_content 'red'
+        click_link I18n.t('views.button.back')
+        click_button I18n.t('views.button.edit')
+        check 'red'
+        click_button I18n.t('helpers.submit.update')
+        click_button I18n.t('views.button.create')
+        expect(page).to have_content 'red'
+      end
+    end
+  end
+
+  describe 'ラベル検索機能' do
+    before do
+    end
+    context 'ラベルを選択して検索する場合' do
+      it '該当するラベルのみ表示される' do
+      end
+    end
   end
 
 end
