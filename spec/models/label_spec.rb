3587fce2479b87b970433e292d6ac8f8e227b809
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-11 10:23:17 +0900

    model label_spec作成

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Label function', type: :model do
+
+    context 'No genre entered' do
+      it 'Get caught in validation' do
+        label = Label.new(genre: "")
+        expect(label).not_to be_valid
+      end
+    end
+
+    context 'Entered more than 10 characters in the genre' do
+      it 'Get caught in validation' do
+        label = Label.new(genre: "a" * 11)
+        expect(label).not_to be_valid
+      end
+    end
+
+    context 'genre is entered' do
+      it 'Pass the validation' do
+        label = Label.new(genre: "sample")
+        expect(label).to be_valid
+      end
+    end
+  end
 end
