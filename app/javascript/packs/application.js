fbd9d35d82e07f73017690d25bfdc6ecb01aa3ac
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-12 23:12:03 +0900

    chartkick gem 導入準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require('jquery')
 require('@nathanvda/cocoon')
 
 import "./calendar/event"
+import "chartkick/chart.js"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,4 +19,3 @@ import "./calendar/event"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
