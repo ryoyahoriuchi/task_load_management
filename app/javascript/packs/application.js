b2a217de5ff42b6d625f74a37402ec13d721d1f4
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-12 19:55:56 +0900

    eventのシステムテストコード作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery')
 require('@nathanvda/cocoon')
 
+import "./calendar/event"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +18,4 @@ require('@nathanvda/cocoon')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -6,8 +6,32 @@ RSpec.describe 'Event function', type: :system do
     visit root_path
   end
 
-  describe 'イベント紐づけ機能' do
-    context 'タスクに合わせて'
-  end
+  describe 'Calendar function' do
 
+    before do
+      click_link I18n.t('views.link.create_task')
+      fill_in 'task[title]', with: 'test_title'
+      fill_in 'task[overview]', with: 'test_overview'
+      fill_in "task[event_attributes][start_time_on]", with: "002022-04-12"
+      fill_in "task[event_attributes][end_time_on]", with: "002022-04-15"
+      click_button I18n.t('helpers.submit.create')
+      click_button I18n.t('views.button.create')
+      click_link I18n.t('views.link.back')
+    end
+
+    context 'Create a new task' do
+      it 'The task will be displayed in the calendar' do
+        calendar = find_by_id("calendar")
+        expect(calendar).to have_content 'test_title'
+      end
+    end
+
+    context 'When deleting a task' do
+      it 'The task is removed from the calendar' do
+        click_button I18n.t('views.button.delete')
+        calendar = find_by_id("calendar")
+        expect(calendar).not_to have_content 'test_title'
+      end
+    end
+  end
 end
