c1529f5b99cfc36c18216ac182d123e9ff39c89b
ryoya horiuchi <appleorange0720@gmail.com>
2022-04-22 13:18:19 +0900

    scopeメソッドのテストコード作成

--- spec/models/task_item_spec.rb
+++ spec/models/task_item_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe TaskItem, type: :model do
       end
     end
   end
+
+  describe 'scope' do
+    let!(:first_user) { FactoryBot.create(:first_user) }
+    let!(:first_task) { FactoryBot.create(:first_task, user: first_user) }
+    let!(:third_task_item) { FactoryBot.create(:third_task_item, task: first_task, created_at: Date.today + 3.days) }
+    let!(:second_task_item) { FactoryBot.create(:second_task_item, task: first_task, created_at: Date.today + 2.days) }
+    let!(:first_task_item) { FactoryBot.create(:first_task_item, task: first_task, created_at: Date.today + 1.days) }
+
+    context 'create_sorted' do
+      it 'Sorted in order of creation' do
+        expect(TaskItem.pluck(:item)).to match ['problem', 'issue02', 'item01']
+        expect(TaskItem.create_sorted.pluck(:item)).to match ['item01', 'issue02', 'problem']
+      end
+    end
+  end
 end
